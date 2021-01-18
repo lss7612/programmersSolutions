fd4c89452a9e4f5c0cd1fb4733d7c2a996bdd74b
ByeongJuP <68850344+ByeongJuP@users.noreply.github.com>
2021-01-18 21:15:39 +0900

    210119_pbj

--- level1/05. 가운데 글자 가져오기/가운데 글자 가져오기_박병주.js
+++ level1/05. 가운데 글자 가져오기/가운데 글자 가져오기_박병주.js
@@ -2,9 +2,9 @@ function solution(s) {
     var answer = '';
     
     //길이가 홀수면 길이/2+1번째 문자
-    console.log(s);
-    console.log(s.length);
-    console.log(s[0]);
+    // console.log(s);
+    // console.log(s.length);
+    // console.log(s[0]);
     
     if( s.length%2 == 0 ){
         answer = s[s.length/2-1 ];
