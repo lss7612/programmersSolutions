d36404ad6600924ee84efcc654dd20ba7eaaf728
lss7612 <lss76121@naver.com>
2021-02-11 17:30:21 +0900

    0211

--- level1/01. 크레인 인형뽑기 게임/easy.js
+++ level1/01. 크레인 인형뽑기 게임/easy.js
@@ -1,17 +1,3 @@
-const transpose = reduce;
-
-function reduce(matrix) {
-  matrix.reduce(reducer(result, row), []);
-}
-function reducer(result, row) {
-  row.map((_, i) => [...(result[i] || [])], row[i]);
-}
-
-const solution = (board, moves) => {
-  const stacks = transpose(board);
-  console.log(stacks);
-};
-
 let board = [
   [0, 0, 0, 0, 0],
   [0, 0, 1, 0, 3],
@@ -19,12 +5,3 @@ let board = [
   [4, 2, 4, 4, 2],
   [3, 5, 1, 3, 1],
 ];
-let board_1 = [
-  [3, 4],
-  [5, 2, 2],
-  [1, 4, 5, 1],
-  [3, 4],
-  [1, 2, 1, 3],
-];
-let moves = [1, 5, 3, 5, 1, 2, 1, 4];
-solution(board, moves);
